6d92d342717106b8b219d8cdbe66cd227a214018
zredhard <zredhard@gmail.com>
2020-12-25 10:52:17 +0800

    create functions.php

---
+++ affiliate/functions.php
@@ -0,0 +1,13 @@
+<?php
+  
+  function kueri_get_affiliate($enterpiser_link_referral){
+    $kry = "SELECT `enterpriser_link_referral`, `enterpriser_first_name`, `enterpriser_last_name` FROM `enterpriser` ";
+    $kry = "WHERE `enterpriser_link_referral` = '".$enterpiser_link_referral."'";
+    
+  }
+  
+  // ALGORITMA REKURSIVE GET AFFILIATE ENTERPRISER
+  // diimplementasi dengan DFS
+  // 3 variabel baru
+
+?>
